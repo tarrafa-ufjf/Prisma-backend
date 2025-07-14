2b7e898ad9f3d813143ccfe2e1328b864f45ff73
Yan-Fonseca <yanfonseca2002@gmail.com>
2025-07-14 17:14:29 -0300

    Consultas funcionando

--- app.py
+++ app.py
@@ -1,21 +1,65 @@
 from flask import request, jsonify, Flask
-from flask_mysql_connector import MySql
+# from flask_mysql_connector import MySql
 from dotenv import load_dotenv
+import pymysql
 from database import db
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "tarrafa_flask_back_2014"
-app.config['SQLALCHEMY_DATABASE_URI']  = os.getenv('MYSQL_ACCESS')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
 
-@app.route("/course/<int:course_id>", methods=["GET"])
+def get_connection():
+    return pymysql.connect(
+        host=os.getenv('MYSQL_HOST'),
+        port=int(os.getenv('MYSQL_GRAD_PORT')),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        db=os.getenv('MYSQL_DATABASE'),
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.route('/courses')
+def get_courses():
+    connector = get_connection()
+    with connector.cursor() as cursor:
+        cursor.execute('''
+            SELECT *
+            FROM mdl_course
+        ''')
+        coursers = cursor.fetchall()
+    connector.close()
+    return jsonify(coursers), 200
+
+@app.route('/course/<int:course_id>')
 def get_course(course_id):
-    return jsonify({}), 200
+    connector = get_connection()
+    with connector.cursor() as cursor:
+        cursor.execute('''
+            SELECT DISTINCT 
+                u.id AS user_id,
+                u.firstname,
+                lh.objectid AS forum_id,
+                FROM_UNIXTIME(lh.timecreated) AS timestamp
+            FROM mdl_logstore_standard_log lh
+            JOIN mdl_user u ON lh.userid = u.id
+            JOIN mdl_user_enrolments ue ON ue.userid = u.id
+            JOIN mdl_enrol e ON e.id = ue.enrolid
+            JOIN mdl_role r ON r.id = e.roleid
+            WHERE lh.courseid = %s
+            AND lh.eventname LIKE '%%course_module_viewed%%'
+            AND lh.component = 'mod_forum'
+            AND r.archetype = 'student';
+        ''', course_id)
+        coursers = cursor.fetchall()
+    connector.close()
+    return jsonify(coursers), 200
+
+
+
+
 
 @app.route("/")
 def hello():

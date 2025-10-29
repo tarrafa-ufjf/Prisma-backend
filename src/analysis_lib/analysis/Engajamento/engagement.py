039b0c967baefab58f2c94123009e9bbed91c63d
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2025-10-29 10:14:23 -0300

    feat: Implementando retoa de aluno indicador engagement -> preciso otimizar

--- src/analysis_lib/analysis/Engajamento/engagement.py
+++ src/analysis_lib/analysis/Engajamento/engagement.py
@@ -5,6 +5,47 @@ class Engagement(Indicator):
     def __init__(self, mapper):
         super().__init__(mapper)
 
+    def student_analysis(self, subject_id, student_id, version, connector):
+        df_posts = self.mapper.get_engagement_data(connector, subject_id, version)
+        df_alunos = self.mapper.get_all_students(connector, subject_id, version)
+
+        df_alunos["subject_id"] = subject_id
+
+        posts_por_usuario = (
+            df_posts.groupby('user_id')['post_id_required']
+            .count().reset_index()
+            .rename(columns={'post_id_required': 'num_posts_required'})
+        )
+
+        df_final = df_alunos.merge(posts_por_usuario, on='user_id', how='left')
+        df_final['num_posts_required'] = df_final['num_posts_required'].fillna(0).astype(int)
+
+        q1 = df_final["num_posts_required"].quantile(0.25)
+        q3 = df_final["num_posts_required"].quantile(0.75)
+        iqr = q3 - q1
+        lim_inf = q1 - 1.5 * iqr
+        lim_sup = q3 + 1.5 * iqr
+
+        def discretize(x):
+            if x <= lim_inf: return "muito_baixo"
+            elif x <= q1:    return "baixo"
+            elif x <= q3:    return "medio"
+            elif x <= lim_sup:return "alto"
+            else:            return "muito_alto"
+
+        df_final["posts_required_label"] = df_final["num_posts_required"].apply(discretize)
+
+        df_final["user_id"] = pd.to_numeric(df_final["user_id"], errors="coerce")
+        sid = pd.to_numeric(student_id, errors="coerce")
+
+        student_df = df_final.loc[df_final["user_id"] == sid]
+
+        if student_df.empty:
+            return None  
+
+        row = student_df.iloc[0].where(pd.notna(student_df.iloc[0]), None).to_dict()
+        return row
+
     def course_analysis(self, subject_id, version, connector):
         df_posts = self.mapper.get_engagement_data(connector, subject_id, version)
         df_alunos = self.mapper.get_all_students(connector, subject_id, version)
@@ -44,7 +85,6 @@ class Engagement(Indicator):
         # Teste
         return df_final
 
-    
     def general_analysis(self, version, connector, analysis_config):
         batch_size = analysis_config["batch_size"]
         processed = analysis_config["processed"]

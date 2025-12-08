7eaf05e1c8f918f4cb72203716247ee5705a0b03
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2025-12-08 14:00:09 -0300

    clear: Limpando código da lógica de indicadores globais anteriores

--- worker/app.py
+++ worker/app.py
@@ -44,50 +44,6 @@ class Worker:
         self.rabbit_admin = rabbit_admin
         self.db_admin = DatabaseAdmin()
         self.analyzer = Analyzer()
-    
-    def select_indicator_analysis(self, analysis_type, connector, version, analysis_config):
-        if analysis_type == "global_analysis_performance":
-            return self.analyzer.general_performance_analysis(connector, version, analysis_config)
-        elif analysis_type == "global_analysis_engagement":
-            return self.analyzer.general_engagement_analysis(connector, version, analysis_config)
-        elif analysis_type == "global_analysis_motivation":
-            return self.analyzer.general_motivation_analysis(connector, version, analysis_config)
-        elif analysis_type == "global_analysis_pedagogic":
-            return self.analyzer.general_pedagogic_analysis(connector, version, analysis_config)
-        elif analysis_type == "global_analysis_cognitive":
-            return self.analyzer.general_cognitive_analysis(connector, version, analysis_config)
-        elif analysis_type == "global_analysis_give_up":
-            return self.analyzer.general_give_up_analysis(connector, version, analysis_config)
-        else:
-            raise ValueError(f"Tipo de análise desconhecido: {analysis_type}")
-
-    def global_analysis(self, message):
-        body = message["body"]
-        analysis_type = body["type"]
-        version = self.db_admin.get_version_in_database(1)
-        message["version"] = version
-        connector = conn.get_connection_with_config(body.get("db_inst_config"))
-
-        if analysis_type not in ANALYSIS_MAP:
-            raise ValueError(f"Tipo de análise desconhecido: {analysis_type}")
-
-        config = body.get("db_inst_config") or body.get("db_config")
-        entry = ANALYSIS_MAP[analysis_type]
-
-        res = self.select_indicator_analysis(analysis_type, connector, version, body.get("analysis_config", {}))
-        
-        if res["processed"] != res["total"]:
-            self.rabbit_admin.publish_message("tasks_to_process", {
-                "name": f"user:{analysis_type}",
-                "body": {
-                    "type": analysis_type,
-                    "db_inst_config": config,
-                    "analysis_config": res
-                },
-                "version": version
-            }, priority=0)
-        else:
-            self.db_admin.update_global_analysis_status(1, entry["status_index"], 'D')
 
     def subject_analysis(self, message):
         body = message["body"]
@@ -321,6 +277,7 @@ class Worker:
             if_exists="append",  
             index=False,
         )
+        
     def discretize_global_indicators(self, institution_id: int = 1):
         engine = self.db_admin.get_connector()
         version = self.db_admin.get_version_in_database(institution_id)

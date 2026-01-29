621b2612ade54fc18463e2796ca647988ec46c51
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2026-01-29 14:28:13 -0300

    Revert "Revert "fix: Adicionando lógica de tutores ativos no período do curso""
    This reverts commit 43f926fbd068ea5c2d5a961ebc3381bda1614565.

--- src/analysis_lib/analysis/Actors/Tutor/tutor_analyzer.py
+++ src/analysis_lib/analysis/Actors/Tutor/tutor_analyzer.py
@@ -11,28 +11,28 @@ class TutorAnalyzer:
     def _not_implemented(self, name: str):
         raise NotImplementedError(f"{name} for tutor is not implemented yet")
 
-    def analysis_response_foruns(self, subject_id, type_query, version, connector, start_at, end_at, user_id=None):
+    def analysis_response_foruns(self, subject_id, type_query, version, connector, start_at, end_at, tutor_ids, user_id=None):
         forums_response = Analysis_Forums_Response(self.mapper)
         if type_query == "user":
             return forums_response.tutors_analysis(subject_id, user_id, version, connector, start_at, end_at)
         if type_query == "subject":
-            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at)
+            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at, tutor_ids)
         raise ValueError("invalid type_query")
     
-    def analysis_login(self, subject_id, type_query, version, connector, start_at, end_at, user_id=None):
+    def analysis_login(self, subject_id, type_query, version, connector, start_at, end_at, tutor_ids, user_id=None):
         forums_response = Analysis_Login(self.mapper)
         if type_query == "user":
             return forums_response.tutors_analysis(subject_id, user_id, version, connector)
         if type_query == "subject":
-            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at)
+            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at, tutor_ids)
         raise ValueError("invalid type_query")
     
-    def analysis_feedback(self, subject_id, type_query, version, connector, start_at, end_at, user_id=None):
+    def analysis_feedback(self, subject_id, type_query, version, connector, start_at, end_at, tutor_ids, user_id=None):
         forums_response = Analysis_Feedback(self.mapper)
         if type_query == "user":
             return forums_response.tutors_analysis(subject_id, user_id, version, connector)
         if type_query == "subject":
-            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at)
+            return forums_response.subject_analysis(subject_id, version, connector, start_at, end_at, tutor_ids)
         raise ValueError("invalid type_query")
     
     def response_foruns_analysis(self, subject_id, type_query, route, version, connector, user_id=None):
